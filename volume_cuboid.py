2dbac670d69fa1a2c922d00e1ac5db422ca37b11
varun1299 <56631564+varun1299@users.noreply.github.com>
2021-10-25 11:55:23 +0530

    Delete volume_cuboid.py

--- volume_cuboid.py
+++
@@ -1,9 +0,0 @@
-def cuboid_volume(l):
-    if type(l) not in [int, float]:
-        raise TypeError("The length of cuboid can only be a valid integer or a float")
-    return(l*l*l)
-
-length = [2,1.1, -2.5, 2j, 'two']
-
-for i in range(len(length)):
-    print ("The volume of cuboid:",cuboid_volume(length[i]))
